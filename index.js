cedadca351ace1b61c3b960b1975ec28c4f9d560
mvtvthailand <sith@mvtv.co.th>
2025-06-15 14:44:40 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+function sum(a, b) {
+  return a + b;
+}
+
+console.log("Sum = " + sum(5, 3));
+
+module.exports = sum;
---
+++ test.js
@@ -0,0 +1,6 @@
+// test.js
+const assert = require('assert');
+const sum = require('./index');
+
+assert.strictEqual(sum(2, 3), 5);
+console.log("Test passed!");
